0f1932fed7d3ceeeca76c86ed8fa0b16d0979cdc
Oscaritoooo02 <oscaragurcia37@gmail.com>
2026-08-03 12:19:39 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Configuración básica de Vite para un proyecto React sin backend ni base de datos.
 export default defineConfig({
   plugins: [react()],
 })
