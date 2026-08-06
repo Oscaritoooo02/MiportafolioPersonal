956301dc9efcfa25c73a98db040c27fd7ff38b07
Oscaritoooo02 <oscaragurcia37@gmail.com>
2026-08-05 22:25:02 -0600

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,6 @@
 import "../styles/hero.css";
 import profile from "../assets/images/profile.jpeg";
-
+import profileImage from "../assets/images/profile.jpeg";
 function Hero() {
   return (
     <section id="inicio" className="hero">
