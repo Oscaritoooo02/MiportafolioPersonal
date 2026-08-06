dd410650683fd24bf5ce9f8e05bf0da729e90461
Oscaritoooo02 <oscaragurcia37@gmail.com>
2026-08-05 22:17:58 -0600

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,33 +1,39 @@
-import '../styles/hero.css'
+import "../styles/hero.css";
+import profile from "../assets/images/profile.jpeg";
 
 function Hero() {
   return (
     <section id="inicio" className="hero">
       <div className="container hero-container">
         <div className="hero-photo-wrapper">
-      
+
           <img
-            src="/src/assets/images/profile.jpeg"
+            src={profile}
             alt="Foto de perfil"
             className="hero-photo"
           />
+
         </div>
 
         <div className="hero-text">
           <p className="hero-greeting">Hola, mi nombre es</p>
           <h1 className="hero-name">Oscar Agurcia</h1>
-          <h2 className="hero-career">Estudiante de Ingeniería en Ciencias Computacionales</h2>
+          <h2 className="hero-career">
+            Estudiante de Ingeniería en Ciencias Computacionales
+          </h2>
+
           <p className="hero-description">
             Bienvenido a mi espacio personal. Aquí comparto un poco sobre mi
             historia, mis intereses y algunos momentos importantes de mi vida.
           </p>
+
           <a href="#sobre-mi" className="btn">
             Conóceme
           </a>
         </div>
       </div>
     </section>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
